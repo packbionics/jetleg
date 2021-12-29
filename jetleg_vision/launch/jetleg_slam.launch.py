0e861752a1d6a13df4615df2e69efaecd80828a0
Jason X <jasonxie@live.unc.edu>
2021-12-28 18:48:44 -0800

    set up slam launch

--- jetleg_vision/launch/jetleg_slam.launch.py
+++ jetleg_vision/launch/jetleg_slam.launch.py
@@ -4,6 +4,8 @@ from ament_index_python import get_package_share_directory
 from launch_ros.actions import Node
 
 from launch import LaunchDescription
+from launch.actions import IncludeLaunchDescription
+from launch.launch_description_sources import PythonLaunchDescriptionSource
 from launch.actions.declare_launch_argument import DeclareLaunchArgument
 from launch.substitutions import TextSubstitution
 from launch.substitutions.launch_configuration import LaunchConfiguration
@@ -12,47 +14,28 @@ from launch.substitutions.launch_configuration import LaunchConfiguration
 def generate_launch_description():
     
     share_dir = get_package_share_directory('jetleg_vision')
-    
-    path_to_vocabulary_launch_arg = DeclareLaunchArgument(
-        "path_to_vocabulary",
-        default_value=TextSubstitution(
-            text=os.path.join(
-                share_dir,
-                "config/ORBvoc.txt"
+
+    stereo_publisher = IncludeLaunchDescription(
+        PythonLaunchDescriptionSource([
+            os.path.join(
+                get_package_share_directory('image_pub'),
+                'launch',
+                'stereo_image_pub.launch.py'
             )
-        )
+        ])
     )
-    path_to_settings_launch_arg = DeclareLaunchArgument(
-        "path_to_settings",
-        default_value=TextSubstitution(
-            text=os.path.join(
-                share_dir,
-                "config/imx21983_slam_config.yaml"
+
+    slam_node = IncludeLaunchDescription(
+        PythonLaunchDescriptionSource([
+            os.path.join(
+                get_package_share_directory('orb_slam2_ros'),
+                'ros/launch',
+                'orb_slam2_imx21983_stereo_launch.py'
             )
-        )
+        ])
     )
-    
-    do_rectify_launch_arg = DeclareLaunchArgument(
-        "do_rectify",
-        default_value=TextSubstitution(text="False")
-    )
-    
+
     return LaunchDescription([
-        path_to_vocabulary_launch_arg,
-        path_to_settings_launch_arg,
-        do_rectify_launch_arg,
-        Node(
-            package='ros2_orbslam',
-            executable='stereo',
-            remappings=[
-                ('/camera/right', '/camera/right/image_raw'),
-                ('/camera/left', '/camera/right/image_raw'),
-            ],
-            parameters=[{
-                'vocabulary_file_path': LaunchConfiguration('path_to_vocabulary'),
-                'settings_file_path': LaunchConfiguration('path_to_settings'),
-                'do_rectify': LaunchConfiguration('do_rectify'),
-            }
-            ]
-        )
+        stereo_publisher,
+        slam_node
     ])
