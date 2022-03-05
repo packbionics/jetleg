dd2604eaffb7fd8d23c05bb2486ec953d797fddd
Anthony Brown <anthonybrown0528@protonmail.com>
2022-03-05 13:46:40 -0500

    improve heightmap generation speed with vectorization

--- jetleg_vision/jetleg_vision/jetleg_pointcloud_proc.py
+++ jetleg_vision/jetleg_vision/jetleg_pointcloud_proc.py
@@ -2,7 +2,7 @@ import rclpy
 from rclpy.node import Node
 import ctypes
 import struct
-from sensor_msgs.msg import PointCloud2, PointField
+from sensor_msgs.msg import PointCloud2, PointField, Image
 from std_msgs.msg import Header
 import matplotlib.pyplot as plt
 
@@ -11,6 +11,8 @@ import cv2
 import threading
 from queue import Queue
 import time
+
+from cv_bridge import CvBridge
 
 
 class PointCloudProcessing(Node):
@@ -24,19 +26,33 @@ class PointCloudProcessing(Node):
             10)
         self.subscription  # prevent unused variable warning
 
+        self.publisher = self.create_publisher(
+            Image,
+            '/heightmap',
+            10
+        )
+
         self.exit_signal = threading.Event()
 
+        self.bridge = CvBridge()
+
         self.img_queue = Queue()
+        self.img = None
+        self.img_is_populated = False
+
+        self.time_start = time.time()
 
     def listener_callback(self, msg):
-
         cloud_array = np.frombuffer(msg.data, dtype=np.float32).reshape((msg.height, msg.width, 4))
 
         heightmap = self.convert_heightmap(cloud_array)
+        self.compute_traversibility(heightmap)
         
     def convert_heightmap(self, cloud_array):
+
         # flatten
         cloud_array = cloud_array.reshape((cloud_array.shape[0] * cloud_array.shape[1], 4))
+
         # remove nan values
         cloud_array = cloud_array[:, :3]
         cloud_array = cloud_array[np.isfinite(cloud_array).any(axis=1)]
@@ -45,10 +61,6 @@ class PointCloudProcessing(Node):
         # cloud array is (N x 3) array, with each row being [x, y, z]
         # sort by x,y coordinates into heightmap image pixels
         
-        cloud_array[:,:2] = np.round(cloud_array[:, :2], decimals=2)
-
-        np.save('/home/anthony/dev_ws/pointcloud_array.npy', cloud_array)
-
         theta_z_upper = 5 * np.pi / 180
 
         # z view restriction
@@ -70,10 +82,25 @@ class PointCloudProcessing(Node):
         map_cols = 40
         heightmap = np.zeros((map_rows,map_cols))
 
+        idx_x = 0
+        idx_y = 0
+
+        map_rows_minus_one = map_rows - 1
+        map_cols_minus_one = map_cols - 1
+
+        x_range = (x_maximum - x_minimum) / map_rows_minus_one
+        y_range = (y_maximum - y_minimum) / map_cols_minus_one
+
+        coord_minimums = np.array([[x_minimum, y_minimum, 0.0]])
+        coord_range = np.array([[x_range, y_range, 1.0]])
+
+        cloud_restricted -= coord_minimums
+        cloud_restricted /= coord_range
+
         for point in cloud_restricted:
             # index is (coordinate-minimum) / pixel spacing
-            idx_x = int((point[0] - x_minimum) / ((x_maximum-x_minimum) / (map_cols-1)))
-            idx_y = int((point[1] - y_minimum) / ((y_maximum-y_minimum) / (map_rows-1)))
+            idx_x = int(point[0])
+            idx_y = int(point[1])
             if heightmap[idx_x, idx_y] == 0:
                 heightmap[idx_x,idx_y] = point[2]
             else:
@@ -90,14 +117,32 @@ class PointCloudProcessing(Node):
         # image dilation with kernel
         heightmap = cv2.dilate(heightmap, kernel, iterations=1)
 
-        self.img_queue.put(heightmap)
+        heightmap_in_bytes = (heightmap*255).astype(np.uint8)
+
+
+        self.get_logger().info(str(heightmap_in_bytes.dtype))
+
+        imgmsg = self.bridge.cv2_to_imgmsg(heightmap_in_bytes)
+        imgmsg.header.frame_id = 'odom'
+        imgmsg.header.stamp = self.get_clock().now().to_msg()
+
+
+        self.publisher.publish(imgmsg)
+
+        #self.img_queue.put(heightmap)
+        self.img = heightmap
+        if not self.img_is_populated:
+            self.img_is_populated = True
+
+        return heightmap
 
     def display_heightmap(self, img_queue):
         while not self.exit_signal.is_set(): 
 
-            if not img_queue.empty():
-                img = img_queue.get()
-                plt.imshow(img)
+            #if not img_queue.empty():
+            #img = img_queue.get()
+            if self.img_is_populated:
+                plt.imshow(self.img)
                 plt.show(block=False)
                 plt.pause(1/30.0)
                 
@@ -124,7 +169,7 @@ def main(args=None):
     node = PointCloudProcessing()
     
     t1 = threading.Thread(target=node.display_heightmap, args=(node.img_queue,))
-    t1.start()
+    #t1.start()
 
     rclpy.spin(node)
 
