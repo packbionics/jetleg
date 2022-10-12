cb3d631dfa2f189fd9b5cf31b4380dbe0137e4ba
jcbynum2 <jcbynum1@gmail.com>
2022-10-12 18:36:27 -0400

    Added PoseStamped subscriber to single_leg_controller

--- jetleg_control/jetleg_control/single_leg_controller.py
+++ jetleg_control/jetleg_control/single_leg_controller.py
@@ -1,17 +1,24 @@
 import rclpy
 from rclpy.node import Node 
 from sensor_msgs.msg import JointState
+from geometry_msgs.msg import PoseStamped
 
 class SingleLegController(Node):
 
     def __init__(self):
         super().__init__('single_leg_controller')
 
-        self.subscriber = self.create_subscription(JointState, 'joint_states', self.callback, 10)
-        self.get_logger().info('Created subscriber')
+        self.jointStateSubscriber = self.create_subscription(JointState, 'joint_states', self.jointStateCallback, 10)
+        self.get_logger().info('Created jointStateSubscriber')
 
-    def callback(self, message):
+        self.poseStampedSubscriber = self.create_subscription(PoseStamped, 'link_states', self.poseStampedCallback, 10)
+        self.get_logger().info('Created poseStampedSubscriber')
+
+    def jointStateCallback(self, message):
         self.get_logger().info(str(message.name))
+
+    def poseStampedCallback(self, message):
+        self.get_logger().info(str(message))
 
 
 def main():
