ae2ea203e1d81e295801178777ead77c0c8ac401
anthonybrown0528 <anthonybrown0528@protonmail.com>
2023-01-18 19:40:56 -0500

    create static transforms for gridmap

--- jetleg_bringup/launch/jetleg_vision_pipeline.launch.py
+++ jetleg_bringup/launch/jetleg_vision_pipeline.launch.py
@@ -40,4 +40,27 @@ def generate_launch_description():
         remappings=[('image', 'traversibility')]
     )
 
-    return LaunchDescription([jetleg_pointcloud_launch, jetleg_pointcloud_proc_cpp, image_to_map_node])
+    static_transform = Node(
+        package='tf2_ros',
+        executable='static_transform_publisher',
+        arguments=['0', '0', '0', '0.0', '1.0', '0.0', '0.0', 'camera_link', 'partial_transformed_map']
+    )
+
+    another_transform = Node(
+        package='tf2_ros',
+        executable='static_transform_publisher',
+        arguments=['0', '0', '0', '-0.5733227', '0.0', '0.0', '0.8193297', 'partial_transformed_map', 'partial_partial_transformed_map']
+    )
+
+    third_transform = Node(
+        package='tf2_ros',
+        executable='static_transform_publisher',
+        arguments=['0', '3', '-1', '0', '0.0', '0.0', '1', 'partial_partial_transformed_map', 'transformed_map']
+    )
+
+    return LaunchDescription([jetleg_pointcloud_launch, 
+                                jetleg_pointcloud_proc_cpp, 
+                                image_to_map_node, 
+                                static_transform, 
+                                another_transform,
+                                third_transform])
