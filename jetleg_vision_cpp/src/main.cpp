3fffaabb772cd0f6c1255a7fe825c826978afe20
anthonybrown0528 <anthonybrown0528@protonmail.com>
2023-02-22 18:09:01 -0500

    remove jetleg_vision_cpp

--- jetleg_vision_cpp/src/main.cpp
+++
@@ -1,9 +0,0 @@
-#include "jetleg_pointcloud_proc.hpp"
-
-int main(int argc, char ** argv) {
-  rclcpp::init(argc, argv);
-  rclcpp::spin(std::make_shared<JetLegPointCloudProc>());
-
-  rclcpp::shutdown();
-  return 0;
-}
