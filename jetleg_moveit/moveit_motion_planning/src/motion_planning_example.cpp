923c36fc4816bac8cf2b893884f4486a5b26b147
anthonybrown0528 <anthonybrown0528@protonmail.com>
2023-03-29 17:24:30 -0400

    add motion plan for returning to previous position

--- jetleg_moveit/moveit_motion_planning/src/motion_planning_example.cpp
+++ jetleg_moveit/moveit_motion_planning/src/motion_planning_example.cpp
@@ -17,16 +17,7 @@ int main(int argc, char* argv[])
   using moveit::planning_interface::MoveGroupInterface;
   auto move_group_interface = MoveGroupInterface(node, "leg");
 
-  // // Set a target Pose
-  // auto const target_pose = [] {
-  //   geometry_msgs::msg::Pose msg;
-  //   msg.orientation.w = 1.0;
-  //   msg.position.x = 0.28;
-  //   msg.position.y = -0.2;
-  //   msg.position.z = 0.5;
-  //   return msg;
-  // }();
-  // move_group_interface.setPoseTarget(target_pose);
+  // MOVE TO TARGET JOINT STATE
 
   // Set a target joint state
   auto const target_joint_state = [] {
@@ -51,6 +42,37 @@ int main(int argc, char* argv[])
   if (success)
   {
     move_group_interface.execute(plan);
+  }
+  else
+  {
+    RCLCPP_ERROR(logger, "Planning failed!");
+  }
+
+  // MOVE BACK TO ORIGINAL STATE
+
+  // Set a target joint state
+  auto const target_joint_state1 = [] {
+    std::map<std::string, double> joint_state;
+
+    joint_state.insert(std::pair<std::string, double>("vertical_rail_to_mount", 0.0));
+    joint_state.insert(std::pair<std::string, double>("knee_joint", 0.0));
+    joint_state.insert(std::pair<std::string, double>("ankle_joint", 0.0));
+
+    return joint_state;
+  }();
+  move_group_interface.setJointValueTarget(target_joint_state1);
+
+  // Create a plan to that target pose
+  auto const [success1, plan1] = [&move_group_interface] {
+    moveit::planning_interface::MoveGroupInterface::Plan msg;
+    auto const ok = static_cast<bool>(move_group_interface.plan(msg));
+    return std::make_pair(ok, msg);
+  }();
+
+  // Execute the plan
+  if (success1)
+  {
+    move_group_interface.execute(plan1);
   }
   else
   {
