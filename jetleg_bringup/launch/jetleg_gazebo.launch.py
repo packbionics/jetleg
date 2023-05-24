dbb05a03622c70a3d7ed9a656e54fb71eedb2193
Anthony Brown <anthonybrown0528@protonmail.com>
2023-05-24 14:37:52 -0400

    modify default behavior of jetleg gazebo launch

--- jetleg_bringup/launch/jetleg_gazebo.launch.py
+++ jetleg_bringup/launch/jetleg_gazebo.launch.py
@@ -21,18 +21,21 @@ import xacro
 
 from ament_index_python.packages import get_package_share_directory
 
+from launch_ros.substitutions import FindPackageShare
 from launch import LaunchDescription
-from launch.actions import IncludeLaunchDescription
+from launch.actions import IncludeLaunchDescription, DeclareLaunchArgument
 from launch.launch_description_sources import PythonLaunchDescriptionSource
 from launch_ros.actions import Node
-
+from launch.conditions import IfCondition
+from launch.substitutions import LaunchConfiguration, PathJoinSubstitution, PythonExpression
 
 def generate_launch_description():
 
-    gazebo_ros_path = get_package_share_directory("gazebo_ros")
+    gazebo_ros_path = FindPackageShare("gazebo_ros")
     jetleg_description_path = get_package_share_directory("jetleg_description")
+    jetleg_bringup_path = FindPackageShare("jetleg_bringup")
 
-    xacro_path = os.path.join(jetleg_description_path, 'urdf/jetleg_wheeled_testrig.urdf.xacro')
+    xacro_path = os.path.join(jetleg_description_path, 'urdf/jetleg_testrig_vision.xacro')
 
     urdf_model = xacro.parse(open(xacro_path))
     xacro.process_doc(urdf_model)
@@ -46,8 +49,31 @@ def generate_launch_description():
 
     spawn_params = model_description + model_pos + model_orientation
 
+    rviz_config = LaunchConfiguration("rviz_config")
+    rviz_toggle = LaunchConfiguration("rviz")
+    gui_toggle = LaunchConfiguration("gui")
+
+    rviz_config_arg = DeclareLaunchArgument(
+        "rviz_config", 
+        default_value=PathJoinSubstitution([jetleg_bringup_path, 'config/jetleg_gazebo.rviz']),
+        description="Specifies the absolute path of the RVIZ config used for default RVIZ visualization"
+    )
+    rviz_toggle_arg = DeclareLaunchArgument(
+        "rviz", 
+        default_value="True",
+        description="Toggles the startup of the RVIZ visualization"
+    )
+    gui_toggle_arg = DeclareLaunchArgument(
+        "gui", 
+        default_value="false",
+        description="Toggles the Gazebo client"
+    )
+
     gazebo = IncludeLaunchDescription(
                 PythonLaunchDescriptionSource([gazebo_ros_path, '/launch/gazebo.launch.py']),
+                launch_arguments={
+                    'gui': gui_toggle
+                }.items()
     )
 
     spawn_entity = Node(package='gazebo_ros', 
@@ -62,11 +88,28 @@ def generate_launch_description():
         parameters=[{'robot_description': urdf_model.toxml()}]
     )
 
+    rviz = Node(
+        package='rviz2',
+        executable='rviz2',
+        arguments=['-d', rviz_config],
+        condition=IfCondition(
+            PythonExpression(
+                [rviz_toggle]
+            )
+        ),
+        output='screen'
+    )
+
     ld = LaunchDescription()
+
+    ld.add_action(rviz_config_arg)
+    ld.add_action(rviz_toggle_arg)
+    ld.add_action(gui_toggle_arg)
 
     ld.add_action(gazebo)
     ld.add_action(rsp)
     ld.add_action(spawn_entity)
+    ld.add_action(rviz)
 
     for controller in controller_list:
         ld.add_action(Node(
