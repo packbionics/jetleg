860f7101721e7040ec4a42f25ac9acd45aa7625e
anthonybrown0528 <anthonybrown0528@protonmail.com>
2023-07-21 20:33:58 -0400

    control intact leg

--- jetleg_control/scripts/jetleg_gait_generator.py
+++ jetleg_control/scripts/jetleg_gait_generator.py
@@ -26,20 +26,29 @@ class JetLegGait(Node):
 
         self.knee_setpoints_quick = np.array([8,18,20,12,10,16,38,60,55,18], dtype=np.float32)
 
-        self.ankle_setpoints_quick = np.array([5,81,75,70,72,80,102,90,80,75], dtype=np.float32)
+        self.ankle_setpoints_quick = np.array([95,81,75,70,72,80,102,90,80,75], dtype=np.float32)
         self.ankle_setpoints_quick = self.ankle_setpoints_quick - 90
                 
         self.T = 4.0
 
-        self.leg_joint_trajectory = JetLegGait.gen_joint_trajectory_from(self.hip_setpoints_quick, self.knee_setpoints_quick, self.ankle_setpoints_quick, self.T)
+        joint_names = ['vertical_rail_to_mount', 'knee_joint', 'ankle_joint']
+        joint_names_intact = ['vertical_rail_to_mount_intact', 'knee_joint_intact', 'ankle_joint_intact']
 
-    def send_goal(self, trajectory: JointTrajectory):
+        self.leg_joint_trajectory = JetLegGait.gen_joint_trajectory_from(joint_names, self.hip_setpoints_quick, self.knee_setpoints_quick, self.ankle_setpoints_quick, self.T)
+        
+        self.hip_setpoints_quick = np.concatenate((self.hip_setpoints_quick[5:], self.hip_setpoints_quick[:5]))
+        self.knee_setpoints_quick = np.concatenate((self.knee_setpoints_quick[5:], self.knee_setpoints_quick[:5]))
+        self.ankle_setpoints_quick = np.concatenate((self.ankle_setpoints_quick[5:], self.ankle_setpoints_quick[:5]))
+        
+        self.leg_intact_joint_trajectory = JetLegGait.gen_joint_trajectory_from(joint_names_intact, self.hip_setpoints_quick, self.knee_setpoints_quick, self.ankle_setpoints_quick, self.T)
+
+    def send_goal(self, trajectory: JointTrajectory, action_client: ActionClient):
         goal_msg = FollowJointTrajectory.Goal()
         goal_msg.trajectory = trajectory
 
-        self.follow_trajectory_client.wait_for_server()
+        action_client.wait_for_server()
 
-        return self.follow_trajectory_client.send_goal_async(goal_msg, feedback_callback=self.feedback_callback)
+        return action_client.send_goal_async(goal_msg, feedback_callback=self.feedback_callback)
 
     def goal_response_callback(self, future):
         goal_handle = future.result()
@@ -58,10 +67,8 @@ class JetLegGait(Node):
 
     def feedback_callback(self, feedback_msg):
         feedback = feedback_msg.feedback
-        # self.get_logger().info('Received feedback: {0}'.format(repr(feedback)))
-        # self.get_logger().info('\n\n')
 
-    def gen_joint_trajectory_from(thigh_setpoints: list, knee_setpoints: list, ankle_setpoints: list, gait_period: float):
+    def gen_joint_trajectory_from(joint_names: list, thigh_setpoints: list, knee_setpoints: list, ankle_setpoints: list, gait_period: float):
         if gait_period <= 0:
             raise ValueError(f"Gait period must be a stricly positive value. Given: {gait_period}")
         
@@ -72,7 +79,8 @@ class JetLegGait(Node):
         
         joint_traj = JointTrajectory()
 
-        joint_traj.joint_names = ['vertical_rail_to_mount', 'knee_joint', 'ankle_joint']
+        # joint_traj.joint_names = ['vertical_rail_to_mount', 'knee_joint', 'ankle_joint']
+        joint_traj.joint_names = joint_names
 
         # number of intervals is the number of setpoints less 1
         time_per_interval = gait_period / (len(thigh_setpoints) - 1)
@@ -96,13 +104,20 @@ def main():
 
     while True:
 
-        future = node.send_goal(node.leg_joint_trajectory)
+        future = node.send_goal(node.leg_joint_trajectory, node.follow_trajectory_client)
         rclpy.spin_until_future_complete(node, future=future)
+
+        future1 = node.send_goal(node.leg_intact_joint_trajectory, node.follow_trajectory_client_intact)
+        rclpy.spin_until_future_complete(node, future=future1)
 
         goal_handle = future.result()
         future = goal_handle.get_result_async()
 
+        goal_handle = future1.result()
+        future1 = goal_handle.get_result_async()
+
         rclpy.spin_until_future_complete(node, future=future)
+        rclpy.spin_until_future_complete(node, future=future1)
 
     node.get_logger().info('Close program')
 
