3f0cfa19e8e4a720952d5280a8dcbcd5462dbeb3
anthonybrown0528 <anthonybrown0528@protonmail.com>
2023-08-17 13:59:28 -0400

    Remove dependence on utility package

--- jetleg_bringup/launch/jetleg_bringup.launch.py
+++ jetleg_bringup/launch/jetleg_bringup.launch.py
@@ -1,16 +1,35 @@
 from launch import LaunchDescription
-from launch.substitutions import PythonExpression
+from launch.actions import DeclareLaunchArgument, IncludeLaunchDescription
+from launch.substitutions import PythonExpression, LaunchConfiguration
+from launch.conditions import IfCondition
+from launch.launch_description_sources import PythonLaunchDescriptionSource
+from launch_ros.substitutions import FindPackageShare
 
-from packbionics_launch_utils.launch_utils import add_launch_argument, add_launch_file
+def add_launch_file(package_name: str, launch_name: str, conditional=None):
+    package = FindPackageShare(package_name)
 
+    launch_condition = None
+    if conditional is not None:
+        launch_condition = IfCondition(
+            conditional
+        )
+
+    launch_file = IncludeLaunchDescription(
+        PythonLaunchDescriptionSource([package, '/launch', '/' + launch_name]),
+        condition=launch_condition
+    )
+
+    return launch_file
 
 def generate_launch_description():
 
-    use_pybullet_env, use_pybullet_env_arg = add_launch_argument(
-        'use_pybullet_env', 
-        default='True', 
-        description="Toggles between using Pybullet or Gazebo for physics simulation"
+    use_pybullet_env_arg = DeclareLaunchArgument(
+        name='use_pybullet_env',
+        default_value='True',
+        description='Toggles between using Pybullet or Gazebo for physics simulation'
     )
+
+    use_pybullet_env = LaunchConfiguration('use_pybullet_env')
 
     gazebo = add_launch_file("jetleg_bringup", 'jetleg_gazebo.launch.py', conditional=PythonExpression(['not ', use_pybullet_env]))
     pybullet = add_launch_file("jetleg_bringup", 'jetleg_pybullet_ros.launch.py', conditional=use_pybullet_env)
