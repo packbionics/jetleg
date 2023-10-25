1efa047fcbdd2fa0deb3e60d693ca5e0419f3c7e
anthonybrown0528 <anthonybrown0528@protonmail.com>
2023-10-25 18:47:17 -0400

    hello github

---
+++ jetleg_control/scripts/Service.py
@@ -0,0 +1,49 @@
+#!/usr/bin/python3
+import sys
+from array import array as array
+from packbionics_interfaces.srv._update_impedance import UpdateImpedance
+import rclpy
+from rclpy.node import Node
+
+
+class MinimalClientAsync(Node):
+
+    def __init__(self):
+        super().__init__('minimal_client_async')
+        self.cli = self.create_client(UpdateImpedance, '/jetleg_controller/impedance_params')
+        while not self.cli.wait_for_service(timeout_sec=1.0):
+            self.get_logger().info('service not available, waiting again...')
+        self.req = UpdateImpedance.Request()
+
+    def send_request(self, stiffness, damping, equilibrium):
+        self.req.stiffness = stiffness
+        self.req.damping = damping
+        self.req.equilibrium = equilibrium
+        self.future = self.cli.call_async(self.req)
+        rclpy.spin_until_future_complete(self, self.future)
+        return self.future.result()
+
+
+def main(args=None):
+    rclpy.init(args=args)
+
+    minimal_client = MinimalClientAsync()
+
+    stiffness = [1.0,1.0,0.0]
+    damping = [1.0 , 1.0 , 0.0]
+    equilibrium = [1.0 , 1.0, 0.0]
+    stiffness = array('d', stiffness)
+    damping = array('d', damping)
+    equilibrium = array('d', equilibrium)
+
+    response = minimal_client.send_request(stiffness, damping, equilibrium)
+    minimal_client.get_logger().info(
+        'Result of add_two_ints: for'  
+        )
+
+    minimal_client.destroy_node()
+    rclpy.shutdown()
+
+
+if __name__ == '__main__':
+    main()
