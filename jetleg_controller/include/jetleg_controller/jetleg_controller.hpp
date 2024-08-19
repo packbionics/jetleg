948de93c898bf46939aa0f3f95d50124307b6d03
anthonybrown0528 <anthonybrown0528@protonmail.com>
2024-08-19 12:13:03 -0400

    [Lint] Address lint failures in jetleg_controller

--- jetleg_controller/include/jetleg_controller/jetleg_controller.hpp
+++ jetleg_controller/include/jetleg_controller/jetleg_controller.hpp
@@ -1,6 +1,28 @@
+// Copyright 2024 Pack Bionics
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
+// THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+// THE SOFTWARE.
+
+
 #ifndef JETLEG_CONTROLLER__JETLEG_CONTROLLER_HPP_
 #define JETLEG_CONTROLLER__JETLEG_CONTROLLER_HPP_
 
+#include <memory>
 #include <rclcpp_action/rclcpp_action.hpp>
 
 #include <controller_interface/controller_interface.hpp>
@@ -11,7 +33,8 @@
 #include "jetleg_controller_parameters.hpp"
 
 
-namespace jetleg_controller {
+namespace jetleg_controller
+{
 
 using FollowJointTrajectoryAction = control_msgs::action::FollowJointTrajectory;
 
@@ -34,19 +57,24 @@ public:
 
   controller_interface::return_type update(
     const rclcpp::Time & time, const rclcpp::Duration & period) override;
+
 private:
-    rclcpp_action::GoalResponse goal_received_callback(const rclcpp_action::GoalUUID &, std::shared_ptr<const FollowJointTrajectoryAction::Goal>);
+  rclcpp_action::GoalResponse goal_received_callback(
+    const rclcpp_action::GoalUUID &,
+    std::shared_ptr<const FollowJointTrajectoryAction::Goal>);
 
-    rclcpp_action::CancelResponse goal_cancelled_callback(std::shared_ptr<rclcpp_action::ServerGoalHandle<FollowJointTrajectoryAction>> goal_handle);
+  rclcpp_action::CancelResponse goal_cancelled_callback(
+    std::shared_ptr<rclcpp_action::ServerGoalHandle<FollowJointTrajectoryAction>> goal_handle);
 
-    void goal_accepted_callback(std::shared_ptr<rclcpp_action::ServerGoalHandle<FollowJointTrajectoryAction>> goal_handle);
+  void goal_accepted_callback(
+    std::shared_ptr<rclcpp_action::ServerGoalHandle<FollowJointTrajectoryAction>> goal_handle);
 
-    rclcpp_action::Server<FollowJointTrajectoryAction>::SharedPtr mActionServer;
+  rclcpp_action::Server<FollowJointTrajectoryAction>::SharedPtr mActionServer;
 
-    std::shared_ptr<jetleg_controller::ParamListener> mParamListener;
-    jetleg_controller::Params mParams;
+  std::shared_ptr<jetleg_controller::ParamListener> mParamListener;
+  jetleg_controller::Params mParams;
 };
 
-}
+}  // namespace jetleg_controller
 
 #endif  // JETLEG_CONTROLLER__JETLEG_CONTROLLER_HPP_
--- jetleg_controller/src/jetleg_controller.cpp
+++ jetleg_controller/src/jetleg_controller.cpp
@@ -1,127 +1,151 @@
+// Copyright 2024 Pack Bionics
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
+// THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+// THE SOFTWARE.
+
+
 #include "jetleg_controller/jetleg_controller.hpp"
 
-namespace jetleg_controller {
+namespace jetleg_controller
+{
 
-  enum class ERROR_TYPE
-  {
-    PARAM_LOAD_FAIL,
-  };
+enum class ERROR_TYPE
+{
+  PARAM_LOAD_FAIL,
+};
 
-  static const std::map<ERROR_TYPE, const std::string> ERROR_MSG_TEMPLATES = {
-    {ERROR_TYPE::PARAM_LOAD_FAIL,
-      "Exception thrown during init stage with message: %s \n"},
-  };
+static const std::map<ERROR_TYPE, const std::string> ERROR_MSG_TEMPLATES = {
+  {ERROR_TYPE::PARAM_LOAD_FAIL,
+    "Exception thrown during init stage with message: %s \n"},
+};
 
-  static const std::string NAMESPACE_SEPARATOR = "/";
-  static const std::string TOPIC_NAME = "follow_joint_trajectory";
+static const char NAMESPACE_SEPARATOR[] = "/";
+static const char TOPIC_NAME[] = "follow_joint_trajectory";
 
 
-  controller_interface::CallbackReturn JetlegController::on_init()
-  {
-    try
-    {
-      // Create the parameter listener and get the parameters
-      mParamListener = std::make_shared<jetleg_controller::ParamListener>(get_node());
-      mParams = mParamListener->get_params();
+controller_interface::CallbackReturn JetlegController::on_init()
+{
+  try {
+    // Create the parameter listener and get the parameters
+    mParamListener = std::make_shared<jetleg_controller::ParamListener>(get_node());
+    mParams = mParamListener->get_params();
+  } catch (const std::exception & e) {
+    RCLCPP_ERROR(
+      get_node()->get_logger(), ERROR_MSG_TEMPLATES.at(
+        ERROR_TYPE::PARAM_LOAD_FAIL).c_str(), e.what());
+    return CallbackReturn::ERROR;
+  }
+
+  return controller_interface::CallbackReturn::SUCCESS;
+}
+
+controller_interface::InterfaceConfiguration JetlegController::command_interface_configuration()
+const
+{
+  controller_interface::InterfaceConfiguration conf;
+  conf.type = controller_interface::interface_configuration_type::INDIVIDUAL;
+
+  for (const auto & joint : mParams.joints) {
+    for (const auto & cmd_if : mParams.command_interfaces) {
+      conf.names.push_back(joint + NAMESPACE_SEPARATOR + cmd_if);
     }
-    catch (const std::exception & e)
-    {
-      RCLCPP_ERROR(get_node()->get_logger(), ERROR_MSG_TEMPLATES.at(ERROR_TYPE::PARAM_LOAD_FAIL).c_str(), e.what());
-      return CallbackReturn::ERROR;
+  }
+
+  return conf;
+}
+
+controller_interface::InterfaceConfiguration JetlegController::state_interface_configuration() const
+{
+  controller_interface::InterfaceConfiguration conf;
+  conf.type = controller_interface::interface_configuration_type::INDIVIDUAL;
+
+  // Specify joint state interfaces from parameters
+  for (const auto & joint : mParams.joints) {
+    for (const auto & cmd_if : mParams.state_interfaces) {
+      conf.names.push_back(joint + NAMESPACE_SEPARATOR + cmd_if);
     }
-
-    return controller_interface::CallbackReturn::SUCCESS;
   }
 
-  controller_interface::InterfaceConfiguration JetlegController::command_interface_configuration() const
-  {
-    controller_interface::InterfaceConfiguration conf;
-    conf.type = controller_interface::interface_configuration_type::INDIVIDUAL;
+  // Specify IMU sensor state interfaces from parameters
+  std::map<std::string, semantic_components::IMUSensor> imuSensors;
+  for (const auto & imu : mParams.imus) {
+    const auto [entry, success] = imuSensors.insert({imu, semantic_components::IMUSensor(imu)});
+    std::vector<std::string> stateInterfaceNames = entry->second.get_state_interface_names();
 
-    for(const auto & joint : mParams.joints) {
-      for (const auto & cmd_if : mParams.command_interfaces) {
-        conf.names.push_back(joint + NAMESPACE_SEPARATOR + cmd_if);
-      }
-    }
-
-    return conf;
+    conf.names.insert(conf.names.end(), stateInterfaceNames.begin(), stateInterfaceNames.end());
   }
 
-  controller_interface::InterfaceConfiguration JetlegController::state_interface_configuration() const
-  {
-    controller_interface::InterfaceConfiguration conf;
-    conf.type = controller_interface::interface_configuration_type::INDIVIDUAL;
+  return conf;
+}
 
-    // Specify joint state interfaces from parameters
-    for(const auto & joint : mParams.joints) {
-      for (const auto & cmd_if : mParams.state_interfaces) {
-        conf.names.push_back(joint + NAMESPACE_SEPARATOR + cmd_if);
-      }
-    }
-    
-    // Specify IMU sensor state interfaces from parameters
-    std::map<std::string, semantic_components::IMUSensor> imuSensors;
-    for(const auto & imu : mParams.imus) {
-      const auto [entry, success] = imuSensors.insert({imu, semantic_components::IMUSensor(imu)});
-      std::vector<std::string> stateInterfaceNames = entry->second.get_state_interface_names();
+controller_interface::CallbackReturn JetlegController::on_configure(
+  const rclcpp_lifecycle::State & /* previous_state */)
+{
+  // Create the action server which will handle motion control requests
+  mActionServer = rclcpp_action::create_server<FollowJointTrajectoryAction>(
+    get_node(),
+    std::string(get_node()->get_name()) + TOPIC_NAME,
+    std::bind(
+      &JetlegController::goal_received_callback, this, std::placeholders::_1,
+      std::placeholders::_2),
+    std::bind(&JetlegController::goal_cancelled_callback, this, std::placeholders::_1),
+    std::bind(&JetlegController::goal_accepted_callback, this, std::placeholders::_1)
+  );
 
-      conf.names.insert(conf.names.end(), stateInterfaceNames.begin(), stateInterfaceNames.end());
-    }
+  return controller_interface::CallbackReturn::SUCCESS;
+}
 
-    return conf;
-  }
-
-  controller_interface::CallbackReturn JetlegController::on_configure(
-    const rclcpp_lifecycle::State & /* previous_state */)
-  {
-
-    // Create the action server which will handle motion control requests
-    mActionServer = rclcpp_action::create_server<FollowJointTrajectoryAction>(
-      get_node(),
-      std::string(get_node()->get_name()) + TOPIC_NAME,
-      std::bind(&JetlegController::goal_received_callback, this, std::placeholders::_1, std::placeholders::_2),
-      std::bind(&JetlegController::goal_cancelled_callback, this, std::placeholders::_1),
-      std::bind(&JetlegController::goal_accepted_callback, this, std::placeholders::_1)
-    );
-
-    return controller_interface::CallbackReturn::SUCCESS;
-  }
-
-  controller_interface::CallbackReturn JetlegController::on_activate(
-    const rclcpp_lifecycle::State & /* previous_state */)
-  {
-    return controller_interface::CallbackReturn::SUCCESS;
-  }
-  controller_interface::CallbackReturn JetlegController::on_deactivate(
-    const rclcpp_lifecycle::State & /* previous_state */)
-  {
-    return controller_interface::CallbackReturn::SUCCESS;
-  }
+controller_interface::CallbackReturn JetlegController::on_activate(
+  const rclcpp_lifecycle::State & /* previous_state */)
+{
+  return controller_interface::CallbackReturn::SUCCESS;
+}
+controller_interface::CallbackReturn JetlegController::on_deactivate(
+  const rclcpp_lifecycle::State & /* previous_state */)
+{
+  return controller_interface::CallbackReturn::SUCCESS;
+}
 
 
-  controller_interface::return_type JetlegController::update(
-    const rclcpp::Time & /* time */, const rclcpp::Duration & /* period */)
-  {
-    return controller_interface::return_type::OK;
-  }
+controller_interface::return_type JetlegController::update(
+  const rclcpp::Time & /* time */, const rclcpp::Duration & /* period */)
+{
+  return controller_interface::return_type::OK;
+}
 
-  rclcpp_action::GoalResponse JetlegController::goal_received_callback(const rclcpp_action::GoalUUID &, std::shared_ptr<const FollowJointTrajectoryAction::Goal>)
-  {
-    return rclcpp_action::GoalResponse::ACCEPT_AND_EXECUTE;
-  }
+rclcpp_action::GoalResponse JetlegController::goal_received_callback(
+  const rclcpp_action::GoalUUID &, std::shared_ptr<const FollowJointTrajectoryAction::Goal>)
+{
+  return rclcpp_action::GoalResponse::ACCEPT_AND_EXECUTE;
+}
 
-  rclcpp_action::CancelResponse JetlegController::goal_cancelled_callback(std::shared_ptr<rclcpp_action::ServerGoalHandle<FollowJointTrajectoryAction>> /* goal_handle */)
-  {
-    return rclcpp_action::CancelResponse::ACCEPT;
-  }
+rclcpp_action::CancelResponse JetlegController::goal_cancelled_callback(
+  std::shared_ptr<rclcpp_action::ServerGoalHandle<FollowJointTrajectoryAction>>/* goal_handle */)
+{
+  return rclcpp_action::CancelResponse::ACCEPT;
+}
 
-  void JetlegController::goal_accepted_callback(std::shared_ptr<rclcpp_action::ServerGoalHandle<FollowJointTrajectoryAction>> /* goal_handle */)
-  {
+void JetlegController::goal_accepted_callback(
+  std::shared_ptr<rclcpp_action::ServerGoalHandle<FollowJointTrajectoryAction>>/* goal_handle */)
+{}
 
-  }
 
-
-}  // jetleg_controller
+}  // namespace jetleg_controller
 
 #include "pluginlib/class_list_macros.hpp"
 
